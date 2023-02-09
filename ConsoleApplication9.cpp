5ab19752eea63e70b3f84cc7f8b69990275cb288
OG_BLUDO <mihailkadyrov2002@mail.ru>
2023-02-09 20:45:05 +0300

    first

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,34 @@
+﻿#include <map>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "ru");
+    map<int, string> scrable =
+    { {10,"Иеруалимов Артём Сергеевич"},
+      {3,"Иван Иванов Иванушкин"},
+      {8,"Денис Денисович Денисов"},
+      {7,"Кирилл Кириллович Кириллов"},
+      {14,"ПОН ПОНОВИЧ ПОНОВ"}
+    };
+    int k = 0;
+    int i=0;
+    cout << "Введите айди: ";
+    cin >> k;
+    for (auto pair : scrable)
+    {
+        if (k <= pair.first)
+        {
+            cout << "Id :" << pair.first << "  Fio :" << pair.second << endl;
+            i++;
+        }
+
+    }
+    cout << "Всего людей удв условию: " << i;
+}
